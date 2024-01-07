0b6a470e4dfc9861c033a5c27b0cd2dcb90ac5df
smroid <smr@dt3.org>
2024-01-06 21:50:35 -0800

    updated readme and setup to be about the fork rather than the original

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open("./requirements.txt", "r") as f:
     requirements = [req.strip().replace(" ", "") for req in f]
 
 if __name__ == "__main__":
-    setup(name="tetra3", version="0.1",
-          description="A fast lost-in-space plate solver for star trackers.",
+    setup(name="cedar-solve", version="0.1",
+          description="A fast lost-in-space plate solver for star trackers. Forked from esa/tetra3.",
           long_description=readme,
           long_description_content_type="text/x-rst",
-          url="https://github.com/esa/tetra3",
-          download_url="https://github.com/esa/tetra3/archive/refs/heads/master.zip",
+          url="https://github.com/smroid/cedar-solve",
+          download_url="https://github.com/smroid/cedar-solve/archive/refs/heads/master.zip",
           packages=find_packages(),
           license="Apache-2.0",
           package_data={"": ["data/*"]},
